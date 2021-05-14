57299777c23dd3ebc1fdfa57588fdfa0c85c41ee
HANSON <80563592+yehanson@users.noreply.github.com>
2021-05-14 09:06:22 -0400

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE ecommerce;
+USE ecommerce;
+CREATE TABLE customers(
+	customer_id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (customer_id),
+    name VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    email VARCHAR(255),
+    phone INT
+);
+CREATE TABLE products(
+	product_id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (product_id),
+    product VARCHAR(255),
+    quantity INT,
+    price INT
+);
+CREATE TABLE orders(
+	order_id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (order_id),
+    customer_id INT NOT NULL,
+    product_id INT NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
+    FOREIGN KEY (product_id) REFERENCES product(product_id),
+    date_ordered DATE,
+    shipping_address VARCHAR(255),
+    quantity INT
+);
